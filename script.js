19b665f30c7783bfedcd283ea1064f34e21e903b
brianrono <brianrono16@gmail.com>
2023-04-16 21:07:37 +0300

    To finish

--- script.js
+++ script.js
@@ -1,120 +1,178 @@
-        // Get DOM elements
-        const startBtn = document.getElementById('start-btn');
-        const nextBtn = document.getElementById('next-btn');
-        const questionContainer = document.getElementById('question-container');
-        const questionElement = document.getElementById('question');
-        const answerButtons = document.getElementById('answer-buttons');
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-        // Add event listener for start button
-        startBtn.addEventListener('click', startQuiz);
+let shuffledQuestions, currentQuestionIndex
 
-        // Add event listener for next button
-        nextBtn.addEventListener('click', () => {
-            currentQuestionIndex++;
-            setNextQuestion();
-        });
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+        setNextQuestion()
+        })
 
-        // Add event listener for answer buttons
-        answerButtons.addEventListener('click', selectAnswer);
-
-        // Quiz data
-        const quizData = [
-            {
-                question: 'Question 1',
-                answers: [
-                    { text: 'Answer 1', correct: false },
-                    { text: 'Answer 2', correct: true },
-                    { text: 'Answer 3', correct: false },
-                    { text: 'Answer 4', correct: false }
-                ]
-            },
-            {
-                question: 'Question 2',
-                answers: [
-                    { text: 'Answer 1', correct: false },
-                    { text: 'Answer 2', correct: false },
-                    { text: 'Answer 3', correct: true },
-                    { text: 'Answer 4', correct: false }
-                ]
-            },
-            // Add more questions as needed
-        ];
-
-        let currentQuestionIndex = 0;
-        let score = 0;
-
-        // Function to start the quiz
-        function startQuiz() {
-            startBtn.classList.add('hide');
-            questionContainer.classList.remove('hide');
-            setNextQuestion();
+    function startGame() {
+        startButton.classList.add('hide')
+        shuffledQuestions = questions.sort(() => Math.random() - .10)
+        currentQuestionIndex = 0
+        questionContainerElement.classList.remove('hide')
+            setNextQuestion()
         }
 
-        // Function to set the next question
-        function setNextQuestion() {
-            resetState();
-            if (currentQuestionIndex < quizData.length) {
-                showQuestion(quizData[currentQuestionIndex]);
-            } else {
-                // Quiz is finished
-                // Show score or perform any other action
-                alert('Quiz finished. Your score: ' + score);
+    function setNextQuestion() {
+        resetState()
+        showQuestion(shuffledQuestions[currentQuestionIndex])
+    }
+
+    function showQuestion(question) {
+        questionElement.innerText = question.question
+        question.answers.forEach(answer => {
+            const button = document.createElement('button')
+            button.innerText = answer.text
+            button.classList.add('btn')
+            if (answer.correct) {
+            button.dataset.correct = answer.correct
             }
-        }
-
-        // Function to show a question
-        function showQuestion(question) {
-            questionElement.innerText = question.question;
-            question.answers.forEach(answer => {
-                const button = document.createElement('button');
-                button.innerText = answer.text;
-                button.classList.add('btn');
-                if (answer.correct) {
-                    button.dataset.correct = true;
-                }
-                button.addEventListener('click', selectAnswer);
-                answerButtons.appendChild(button);
-                });
-                }
-            // Function to handle answer selection
-    function selectAnswer(event) {
-        const selectedButton = event.target;
-        const correct = selectedButton.dataset.correct;
-        setStatusClass(document.body, correct);
-        Array.from(answerButtons.children).forEach(button => {
-            setStatusClass(button, button.dataset.correct);
-        });
-        if (correct) {
-            score++;
-        }
-        if (currentQuestionIndex < quizData.length - 1) {
-            nextBtn.classList.remove('hide');
-        } else {
-            nextBtn.innerText = 'Finish';
-        }
+            button.addEventListener('click', selectAnswer)
+            answerButtonsElement.appendChild(button)
+        })
     }
 
-    // Function to reset state
     function resetState() {
-        nextBtn.classList.add('hide');
-        while (answerButtons.firstChild) {
-            answerButtons.removeChild(answerButtons.firstChild);
+        clearStatusClass(document.body)
+        nextButton.classList.add('hide')
+        while (answerButtonsElement.firstChild) {
+            answerButtonsElement.removeChild(answerButtonsElement.firstChild)
         }
     }
 
-    // Function to set status class
-    function setStatusClass(element, correct) {
-        clearStatusClass(element);
-        if (correct) {
-            element.classList.add('correct');
+    function selectAnswer(e) {
+        const selectedButton = e.target
+        const correct = selectedButton.dataset.correct
+            setStatusClass(document.body, correct)
+            Array.from(answerButtonsElement.children).forEach(button => {
+                setStatusClass(button, button.dataset.correct)
+            })
+        if (shuffledQuestions.length > currentQuestionIndex + 1) {
+            nextButton.classList.remove('hide')
         } else {
-            element.classList.add('incorrect');
+            startButton.innerText = 'Restart'
+            startButton.classList.remove('hide')
         }
     }
 
-    // Function to clear status class
-    function clearStatusClass(element) {
-        element.classList.remove('correct');
-        element.classList.remove('incorrect');
+    function setStatusClass(element, correct) {
+        clearStatusClass(element)
+        if (correct) {
+            element.classList.add('correct')
+        } else {
+            element.classList.add('wrong')
+        }
     }
 
+    function clearStatusClass(element) {
+        element.classList.remove('correct')
+        element.classList.remove('wrong')
+    }
+
+    const questions = [
+        {
+            question: 'What is 2 + 2?',
+            answers: [
+                { text: '4', correct: true },
+                { text: '22', correct: false }
+            ]
+        },
+        {
+            question: 'Who is the best YouTuber?',
+            answers: [
+                { text: 'Web Dev Simplified', correct: true },
+                { text: 'Traversy Media', correct: true },
+                { text: 'Dev Ed', correct: true },
+                { text: 'Fun Fun Function', correct: true }
+            ]
+        },
+        {
+            question: 'Is web development fun?',
+            answers: [
+                { text: 'Kinda', correct: false },
+                { text: 'YES!!!', correct: true },
+                { text: 'Um no', correct: false },
+                { text: 'IDK', correct: false }
+            ]
+        },
+        {
+            question: 'What is 4 * 2?',
+            answers: [
+                { text: '6', correct: false },
+                { text: '8', correct: true }
+            ]
+        },
+        {
+            question: "What is the capital of France?",
+            answers: [
+                {text: 'Paris', correct: true },
+                {text: 'Berlin', correct: false },
+                {text: 'Rome', correct: false },
+                {text: 'Madrid', correct: false },
+            ]            
+        },
+        {
+            question: "What is the largest planet in our solar system?",
+            answers: [
+                {text: 'Earth', correct: false},
+                {text: 'Jupiter', correct: true},
+                {text: 'Mars', correct: false},
+                {text: 'Saturn', correct: false},
+            ]
+        },
+        {
+            question: "What is the chemical symbol for gold?",
+            answers: [
+                {text: 'Au', correct: true},
+                {text: 'Ag', correct: false},
+                {text: 'Cu', correct: false},
+                {text: 'Fe', correct: false},
+            ]
+        },
+        {
+            question: "What is the highest mountain in the world?",
+            answers: [
+                {text: 'Mount Fuji', correct: true },
+                {text: 'Mount Kilimanjaro', correct: false },
+                {text: 'Mount Everest', correct: true },
+                {text: 'McKinley', correct: false },                
+            ]
+        },
+        {
+            question: "What is the largest ocean on Earth?",
+            options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+            answer: "Pacific Ocean"
+        },
+        {
+            question: "What is the primary gas that makes up Earth's atmosphere?",
+            options: ["Oxygen", "Nitrogen", "Carbon dioxide", "Methane"],
+            answer: "Nitrogen"
+        },
+        {
+            question: "Who is the author of the Harry Potter book series?",
+            options: ["J.K. Rowling", "Stephen King", "George R.R. Martin", "J.R.R. Tolkien"],
+            answer: "J.K. Rowling"
+        },
+        {
+            question: "What is the largest mammal?",
+            options: ["Elephant", "Blue whale", "Giraffe", "Hippopotamus"],
+            answer: "Blue whale"
+        },
+        {
+            question: "What is the currency of Japan?",
+            options: ["Yen", "Euro", "Dollar", "Pound"],
+            answer: "Yen"
+        },
+        {
+            question: "What is the tallest breed of dog?",
+            options: ["Labrador Retriever", "Great Dane", "Siberian Husky", "Chihuahua"],
+            answer: "Great Dane"
+        }
+    ]
